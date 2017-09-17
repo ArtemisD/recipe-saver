9e463d08bb2ebd43282ee94eee50d8108b77f3b8
ArtemisD <dianah456@gmail.com>
2017-09-17 17:09:55 -0400

    added mongolab

--- server.js
+++ server.js
@@ -8,12 +8,6 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
 app.use(express.static(__dirname + '/public'));
 
 app.use(bodyParser.json());
