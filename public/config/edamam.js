8276fc6b5047cd1f75e2ffc87cc0f40a5ca7f518
ArtemisD <dianah456@gmail.com>
2017-09-17 23:05:28 -0400

    minor update to api

--- public/config/edamam.js
+++ public/config/edamam.js
@@ -1,4 +1,2 @@
 window.API_KEY = '3bc60c0cabaced5cc83ffb23ab6c48d3';
-window.API_ID = '6ef698c7'
-heroku config:set API_KEY="3bc60c0cabaced5cc83ffb23ab6c48d3"
-heroku config:set API_ID="6ef698c7"
+window.API_ID = '6ef698c7';
