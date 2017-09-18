6bbf7e4e9bc45778897a94db8fcb71e4f45108a5
ArtemisD <dianah456@gmail.com>
2017-09-17 23:00:29 -0400

    added process.env

--- public/components/App.jsx
+++ public/components/App.jsx
@@ -12,8 +12,8 @@ class App extends React.Component {
 
   componentDidMount() {
     searchRecipes({
-      api_key: ENV["API_KEY"],
-      api_id: ENV["API_ID"],
+      api_key: process.env.API_KEY,
+      api_id: process.env.API_ID,
       q: 'key lime'
     }, data => {
       this.setState({list: data.hits});
